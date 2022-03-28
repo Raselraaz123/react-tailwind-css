b5d8ca50461e9da32e9d7a862ce0f725c568fed0
Raselraaz123 <raselraaz91@gmail.com>
2022-03-28 12:48:16 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-5xl font-bold mb-3 text-lime-600">
+        save most of your money
+      </h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+        cumque ullam sint et.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
